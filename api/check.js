17a4abcddce79e807df82526e3ff4fb7e67db119
PandazWare <pandazware@gmail.com>
2026-01-12 15:16:09 +0100

    Create check.js

---
+++ api/check.js
@@ -0,0 +1,38 @@
+export default async function handler(req, res) {
+    const { key, hwid } = req.query;
+
+    const r = await fetch(`${process.env.REDIS_URL}/get/${key}`, {
+        headers: {
+            Authorization: `Bearer ${process.env.REDIS_TOKEN}`
+        }
+    });
+
+    const data = await r.json();
+    if (!data.result) {
+        return res.json({ success: false });
+    }
+
+    const value = JSON.parse(data.result);
+
+    if (!value.hwid) {
+        value.hwid = hwid;
+
+        await fetch(`${process.env.REDIS_URL}/set/${key}`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${process.env.REDIS_TOKEN}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                value: JSON.stringify(value),
+                ttl: 24 * 60 * 60
+            })
+        });
+    }
+
+    if (value.hwid !== hwid) {
+        return res.json({ success: false });
+    }
+
+    res.json({ success: true });
+}
